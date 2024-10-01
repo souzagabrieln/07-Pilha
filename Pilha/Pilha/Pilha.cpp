5d2d5acb9b40bbca2f6b1a44b24c3610ec8463ba
souzagabrieln <souzagabriel182@gmail.com>
2024-10-01 11:15:07 -0300

    Resolução da atividade

--- Pilha/Pilha/Pilha.cpp
+++ Pilha/Pilha/Pilha.cpp
@@ -87,13 +87,34 @@ void push()
 	cin >> novo->valor;
 	novo->prox = NULL;
 
+	if (topo != NULL) {
+		novo->prox = topo;
+		topo = novo;
+	}
+	else {
+		topo = novo;
+	}
 
 }
 
 void pop()
 {
+	NO* aux = topo;
 
-	
+	if (topo == NULL) {
+		cout << "Pilha vazia. \n";
+		return;
+	}
+	else if (topo->prox != NULL) {
+		topo = topo->prox;
+		cout << aux->valor <<endl;
+		free(aux);
+	}
+	else {
+		cout << aux->valor << endl;
+		topo = NULL;
+		free(aux);
+	}
 
 }
 
